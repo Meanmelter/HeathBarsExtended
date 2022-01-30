d0fd9d21c7760dc61929df8e2bc6f79eb271dd4e
Meanmelter <meanmelter@yahoo.com>
2022-01-30 01:14:51 -0500

    Update Option Menu text

--- Scripts/HealthBarOptions.lua
+++ Scripts/HealthBarOptions.lua
@@ -3,5 +3,5 @@ function onInit()
 end
 
 function AddHealthBarOptions()
-    OptionsManager.registerOption2("TGMH", false, "option_header_token", "option_label_TGMH", "option_entry_cycler", { labels = "option_val_MBar1|option_val_MBar2", values="MBar1|Mbar2"});
+    OptionsManager.registerOption2("TGMH", false, "option_header_token", "option_label_TGMH", "option_entry_cycler", { labels = "option_val_MBar1|option_val_MBar2", values="Extension Bar|Extension Bar Alternate"});
 end
