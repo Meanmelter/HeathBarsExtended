42b336cfc2e4d3811b91aaed1b9c4ff6905fd1cc
Meanmelter <meanmelter@yahoo.com>
2022-02-01 01:33:21 -0500

    Separate options and token

--- Scripts/HealthBarOptions.lua
+++ Scripts/HealthBarOptions.lua
@@ -3,5 +3,6 @@ function onInit()
 end
 
 function AddHealthBarOptions()
-    OptionsManager.registerOption2("TGMH", false, "option_header_token", "option_label_TGMH", "option_entry_cycler", { labels = "option_val_MBar1|option_val_MBar2", values="Extension Bar|Extension Bar Alternate"});
+    OptionsManager.registerOption2("TGMH", false, "option_header_token", "option_label_TGMH", "option_entry_cycler", { labels = "option_val_MBar1|option_val_MBar2|option_val_bar|option_val_barhover|option_val_dot|option_val_dothover", values="baralt|baralt2|bar|barhover|dot|dothover", baselabel = "option_val_off", baseval = "off", default = "bar"});
 end
+
--- Scripts/HealthBarToken.lua
+++ Scripts/HealthBarToken.lua
@@ -1,4 +1,73 @@
  
 function onInit()
-    Debug.console("Test")
+    TokenManager.updateHealthHelper = updateHealthHelper;
+end
+
+function updateHealthHelper(tokenCT, nodeCT)
+	local sOptTH;
+	if Session.IsHost then
+		sOptTH = OptionsManager.getOption("TGMH");
+	elseif DB.getValue(nodeCT, "friendfoe", "") == "friend" then
+		sOptTH = OptionsManager.getOption("TPCH");
+	else
+		sOptTH = OptionsManager.getOption("TNPCH");
+	end
+	local aWidgets = TokenManager.getWidgetList(tokenCT, "health");
+
+	if sOptTH == "off" then
+		for _,vWidget in pairs(aWidgets) do
+			vWidget.destroy();
+		end
+	else
+		local nPercentWounded,sStatus,sColor = fGetHealthInfo(nodeCT);
+
+		if sOptTH == "bar" or sOptTH == "barhover" then
+			local w, h = tokenCT.getSize();
+
+			local bAddBar = false;
+			if h > 0 then
+				bAddBar = true;
+			end
+
+			if bAddBar then
+				local widgetHealthBar = aWidgets["healthbar"];
+				if not widgetHealthBar then
+					widgetHealthBar = tokenCT.addBitmapWidget("healthbar");
+					widgetHealthBar.sendToBack();
+					widgetHealthBar.setName("healthbar");
+				end
+				if widgetHealthBar then
+					widgetHealthBar.sendToBack();
+					widgetHealthBar.setColor(sColor);
+					widgetHealthBar.setTooltipText(sStatus);
+					widgetHealthBar.setVisible(sOptTH == "bar");
+					TokenManager.updateHealthBarScale(tokenCT, nodeCT, nPercentWounded);
+				end
+			end
+
+			if aWidgets["healthdot"] then
+				aWidgets["healthdot"].destroy();
+			end
+		elseif sOptTH == "dot" or sOptTH == "dothover" then
+			local widgetHealthDot = aWidgets["healthdot"];
+			if not widgetHealthDot then
+				widgetHealthDot = tokenCT.addBitmapWidget("healthdot");
+				widgetHealthDot.setPosition("bottomright", TOKEN_HEALTHDOT_HOFFSET, TOKEN_HEALTHDOT_VOFFSET);
+				widgetHealthDot.setName("healthdot");
+
+				local nDU = GameSystem.getDistanceUnitsPerGrid();
+				local nSpace = math.ceil(DB.getValue(nodeCT, "space", nDU) / nDU);
+				widgetHealthDot.setSize(TOKEN_HEALTHDOT_SIZE * nSpace, TOKEN_HEALTHDOT_SIZE * nSpace);
+			end
+			if widgetHealthDot then
+				widgetHealthDot.setColor(sColor);
+				widgetHealthDot.setTooltipText(sStatus);
+				widgetHealthDot.setVisible(sOptTH == "dot");
+			end
+
+			if aWidgets["healthbar"] then
+				aWidgets["healthbar"].destroy();
+			end
+		end
+	end
 end
